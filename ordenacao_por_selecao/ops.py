892a476b809d3baa45525a9f5defd023b4fc9896
GRS852 <guilherme.ribeiro.souza@hotmail.com>
2024-12-16 19:24:58 -0300

    Ordenação por Seleção

---
+++ ordenacao_por_selecao/ops.py
@@ -0,0 +1,27 @@
+
+
+
+def buscaMenor(arr):
+
+    menor = arr[0]
+    menor_indice = 0
+
+    for i in range(1, len(arr)):
+        print(f"Esse é o I: {i}")
+        if arr[i] < menor:
+
+            print(f"esse é o array: {arr[i]}")
+            menor = arr[i]
+            menor_indice = i
+
+    return menor_indice
+
+
+def ordenacaoporSelecao(arr):
+    novoArr = []
+    for i in range(len(arr)):
+        menor = buscaMenor(arr)
+        novoArr.append(arr.pop(menor))
+    return novoArr
+
+print(ordenacaoporSelecao([5, 3 ,6, 2, 10]))
